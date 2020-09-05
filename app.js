903e216f21a9637f59ef6d7a30b526437aae867d
harshadvict <harshadvict@gmail.com>
2020-09-05 11:49:42 +0530

    random stone falling applied

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ var heatingdesignid = "";
 var egg_design_remove = "";
 var stonename = "";
 var imagechange = "";
-var animtime = 5;
+var animtime = 1200;
+var stonetemp = "";
+let stonearray = ["stone1", "stone2", "stone3", "stone4", "stone5"];
+let heatingarray = [
+  "clicktohatch1",
+  "clicktohatch2",
+  "clicktohatch3",
+  "clicktohatch4",
+  "clicktohatch5",
+];
+let eggdesignarray = [
+  "eggbreak1",
+  "eggbreak2",
+  "eggbreak3",
+  "eggbreak4",
+  "eggbreak5",
+];
+let destroyedimgarray = [
+  "imgchange1",
+  "imgchange2",
+  "imgchange3",
+  "imgchange4",
+  "imgchange5",
+];
+let hatcharray = ["hatched1", "hatched2", "hatched3", "hatched4", "hatched5"];
 function change_heating_color(cname, idtemp, cnum, newtemp) {
   console.log(idtemp);
   //   document.getElementById(idtemp).className = "egg_heating_design_hatch";
@@ -12,12 +36,12 @@ function change_heating_color(cname, idtemp, cnum, newtemp) {
   // // // document.getElementById(idtemp).style.gridColumnStart = cnum;
   // // // document.getElementById(idtemp).style.gridColumnEnd = cnum + 1;
   var y = document.getElementById(newtemp);
-  // y.style.visibility = "hidden";
+  y.style.zIndex = -1;
   var x = document.getElementById(idtemp);
   x.style.display = "block";
   x.style.visibility = "visible";
-  x.style.animation = "heating_the_egg 8s";
-  // x.style.animationPlayState = "paused";
+  x.style.animation = "heating_the_egg 5s";
+  x.style.animationPlayState = "running";
   // var k = 0;
   // setInterval(() => {
   //   var x = document.getElementById(idtemp);
@@ -32,7 +56,8 @@ function change_heating_color(cname, idtemp, cnum, newtemp) {
   //   } else clearInterval();
   // }, 1000);
   //x.addEventListener("webkitAnimationStart", stone_falling_start);
-  x.addEventListener("webkitAnimationEnd", testing());
+  x.addEventListener("webkitAnimationEnd", testing);
+  // scoretheplayer();
 }
 // function stop_heating_color() {
 
@@ -41,7 +66,6 @@ function change_heating_color(cname, idtemp, cnum, newtemp) {
 function change_heating_color1(temp, temp1, hatchtemp, stonetemp, newtemp1) {
   score = 0;
   document.getElementById("score").innerHTML = score;
-  timer();
   imagechange = newtemp1;
   stonename = stonetemp;
   egg_design_remove = temp1;
@@ -137,6 +161,7 @@ function testing() {
   var y = document.getElementById(egg_design_remove);
   console.log(y);
   y.style.visibility = "visible";
+  scoretheplayer();
 }
 
 // score of the player
@@ -167,23 +192,45 @@ function scoretheplayer(hatchtemp) {
 
 // move stone to prevent collision
 
-function movethestone() {
-  var elem = document.getElementById(stonename);
+function movethestone(tempstone) {
+  // var elem1 = document.getElementById(stonetemp);
+  // elem1.style.animationPlayState = "paused";
+  // console.log(elem1);
+  var elem = document.getElementById(tempstone);
   console.log(elem);
-  elem.style.animation = "prevent_collision 5s";
+  elem.style.animation = "prevent_collision 1s";
   elem.style.webkitAnimationFillMode = "forwards";
+  elem.addEventListener("webkitAnimationEnd", resetthe);
+
   //   elem.addEventListener("webkitAnimationStart", stone_falling_start);
-  elem.addEventListener("webkitAnimationEnd", scoretheplayer);
+  // elem.addEventListener("webkitAnimationEnd", scoretheplayer);
+}
+
+function resetthe() {
+  var x = document.getElementById(heatingdesignid);
+  console.log(x);
+  x.style.visibility = "visible";
+  var y = document.getElementById(egg_design_remove);
+  console.log(y);
+  y.style.visibility = "visible";
+  var z = document.getElementById(hatchimage);
+  console.log(z);
+  z.style.display = "none";
 }
 
 // stone falling start
 function stone_falling_start() {
-  var elem = document.getElementById(stonename);
+  console.log(stonetemp);
+  var elem = document.getElementById(stonetemp);
   console.log(elem);
   elem.style.animation = "falling_of_stone";
   elem.style.webkitAnimationFillMode = "forwards";
-  elem.style.webkitAnimationDuration = animtime + "s";
-  animtime = animtime - 1;
+  elem.style.webkitAnimationDuration = animtime + "ms";
+  animtime = animtime - 50;
+  console.log(elem.style.animationPlayState);
+  // if (elem.style.animationPlayState == "paused") {
+  //   return;
+  // }
   //   elem.addEventListener("webkitAnimationStart", stone_falling_start);
   elem.addEventListener("webkitAnimationEnd", eggdestroyed);
 }
@@ -196,7 +243,8 @@ function eggdestroyed() {
   var y = document.getElementById(egg_design_remove);
   console.log(y);
   y.style.visibility = "hidden";
-
+  var a = document.getElementById(stonetemp);
+  a.style.visibility = "hidden";
   console.log("check");
   console.log(imagechange);
   var z = document.getElementById(hatchimage);
@@ -222,4 +270,68 @@ function start_the_game(temp) {
   elem2.style.visibility = "visible";
   var elem3 = document.getElementById("instruction");
   elem3.style.display = "none";
+}
+
+// random fall of stone
+function randomtry() {
+  var i = 0;
+  var myvar = setInterval(function () {
+    i = i + 1;
+    if (i === 5) {
+      clearInterval(myvar);
+    }
+    random_fall();
+  }, 3000);
+}
+function random_fall() {
+  var i;
+  var j;
+  var heat;
+  var eggdesign;
+  var hatch;
+  var destroyedegg;
+  let stone = stonearray[Math.floor(Math.random() * stonearray.length)];
+  for (i = 0; i < stonearray.length; i++) {
+    if (stonearray[i] == stone) {
+      console.log(stone);
+      stonearray.splice(i, 1);
+      j = i;
+    }
+  }
+  for (var i = 0; i < heatingarray.length; i++) {
+    if (i === j) {
+      heat = heatingarray[i];
+      heatingarray.splice(i, 1);
+      break;
+    }
+  }
+  for (var i = 0; i < hatcharray.length; i++) {
+    if (i === j) {
+      hatch = hatcharray[i];
+      hatcharray.splice(i, 1);
+      break;
+    }
+  }
+  for (var i = 0; i < eggdesignarray.length; i++) {
+    if (i === j) {
+      eggdesign = eggdesignarray[i];
+      eggdesignarray.splice(i, 1);
+      break;
+    }
+  }
+  for (var i = 0; i < destroyedimgarray.length; i++) {
+    if (i === j) {
+      destroyedegg = destroyedimgarray[i];
+      destroyedimgarray.splice(i, 1);
+      break;
+    }
+  }
+  heatingdesignid = heat;
+  egg_design_remove = eggdesign;
+  stonetemp = stone;
+  hatchimage = hatch;
+  imagechange = destroyedegg;
+  console.log(stonearray);
+  stone_falling_start();
+  return;
 }
